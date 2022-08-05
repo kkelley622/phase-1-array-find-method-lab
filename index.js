09ab2db37ed83b45284940f573ed39aa5e69a816
Kevin Kelley <kkelley622@gmail.com>
2022-08-05 10:45:12 -0500

    commit tree d3a0a022bcc29ea7259bde8e934570a44b937997

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(record) {
+    // return year Broncos won super bowl
+    const winningSeason = record.find(record => record.result === "W")
+
+    // returns undefined if no win object exists
+    return winningSeason ? winningSeason.year : undefined;
+}
